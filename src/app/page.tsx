6f7ab8ed21e342ae2f252aeebeaea1064bf445b3
Jubayer Al Mahmud <jubayer8221@gmail.com@users.noreply.github.com>
2025-02-13 00:33:35 +0600

    page/home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import ProductPage from "@/components/ProductDetails/ProductDetails";
 import PopularCategories from "@/app/popularCategory/page";
 import PopularProductCard from "@/app/popularProduct/page";
 
-export default function Home() {
+export default function page() {
   return (
     <div>
       <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]"></div>
