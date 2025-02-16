bc913e02e8dc562398f9670b0123073ce45f8002
mdnurulislam12 <nurulislam86061907@gmail.com>
2025-02-16 11:37:51 -0800

    change

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import React from 'react'
 import Banner from './Banner'
 import BBanner from './BBanner'
@@ -6,24 +6,16 @@ import Hotdeals from './Hotdeals'
 import FeaturedProducts from './FeaturedProducts'
 import FeadBack from './FeadBack'
 import Blog from './Blog'
-=======
-import React from "react";
-import Banner from "@/components/Home/Banner";
-import BBanner from "@/components/Home/BBanner";
->>>>>>> 795a3960f7d19d560008dac981a7296d5402a228
 
 const Home = () => {
   return (
     <div className="w-full">
       <Banner />
       <BBanner />
-<<<<<<< HEAD
       <Hotdeals />
       <FeaturedProducts />
       <FeadBack />
       <Blog />
-=======
->>>>>>> 795a3960f7d19d560008dac981a7296d5402a228
     </div>
   );
 };
--- src/components/Home/Hotdeals.tsx
+++ src/components/Home/Hotdeals.tsx
@@ -106,12 +106,9 @@ const hotDealsProduct = [
 ];
 
 const Hotdeals = () => {
-<<<<<<< HEAD
-    
-=======
+
   // const [isHover, setIsHover] = useState(false);
 
->>>>>>> 795a3960f7d19d560008dac981a7296d5402a228
   return (
     <div className="pl-3 pr-3 sm:pl-[100px] sm:pr-[100px] md:pl[200px] md:pr[200px] xl:pl-[300px] xl:pr-[300px] pt-24 pb-24 mt-14 bg-[#F7F7F7] font-poppins">
       <div className="mb-8 flex items-center justify-between">
@@ -124,14 +121,13 @@ const Hotdeals = () => {
       </div>
       {/* hot list */}
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-5 ">
-<<<<<<< HEAD
+
         {
             hotDealsProduct.map(product => (
                 <HotdealsCart key={product.id} img={product.image} title={product.name} price={product.price} rating={product.rating} sale={product.sale ?? ""} oldPrice={product.oldPrice ?? ""} bestSale={product.bestSale} 
                 reviews={product.reviews ?? 0} />
             ))
         }
-=======
         {hotDealsProduct.map((product) => (
           <HotdealsCart
             key={product.id}
@@ -145,7 +141,6 @@ const Hotdeals = () => {
             reviews={product.reviews ?? 0}
           />
         ))}
->>>>>>> 795a3960f7d19d560008dac981a7296d5402a228
       </div>
     </div>
   );
