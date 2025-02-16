cdc128b28f028500795794815194887141571082
mdnurulislam12 <nurulislam86061907@gmail.com>
2025-02-15 23:42:16 -0800

    letest news update

--- src/components/Home/LatestNews.tsx
+++ src/components/Home/LatestNews.tsx
@@ -10,7 +10,7 @@ const LatestNews = () => {
         </h2>
       </div>
       {/* cart  */}
-      <div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 xl:grid-cols-3 gap-5">
         <LatestNewsCart />
       </div>
     </div>
