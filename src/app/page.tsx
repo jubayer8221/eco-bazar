a6c96d7cff5436c543a72166ddf2d5b1e9b6d4b9
Jubayer Al Mahmud <jubayer8221@gmail.com@users.noreply.github.com>
2025-02-17 19:31:59 +0600

    Shopping Cart

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Home from "@/components/Home/Home";
 import WishList from "@/components/Wishlist/page";
+import ShoppingCart from "@/components/shoppingCart/ShoppingCart";
 
 export default function page() {
   return (
     <div>
       <Home />
       <WishList></WishList>
+      <ShoppingCart />
     </div>
   );
 }
