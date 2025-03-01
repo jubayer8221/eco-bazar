79cfa0899b534eed8135bdbba3d6a1f44762c0ac
Jubayer Al Mahmud <jubayer8221@gmail.com@users.noreply.github.com>
2025-03-01 21:24:24 +0600

    add more

---
+++ src/app/innerpage/blog/page.tsx
@@ -0,0 +1,12 @@
+import Blog from "@/components/Home/Blog";
+import LatestNews from "@/components/Home/LatestNews";
+import React from "react";
+
+export default function page() {
+  return (
+    <div>
+      <Blog></Blog>
+      <LatestNews></LatestNews>
+    </div>
+  );
+}
